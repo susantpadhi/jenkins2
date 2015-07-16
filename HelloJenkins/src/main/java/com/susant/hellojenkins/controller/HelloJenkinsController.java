773c9e7c6064ac244731567696b631d42f35583e
Susanta Padhi <susanta.padhi@ge.com>
2015-07-15 22:22:15 -0500

    first commit

---
+++ HelloJenkins/src/main/java/com/susant/hellojenkins/controller/HelloJenkinsController.java
@@ -0,0 +1,23 @@
+package com.susant.hellojenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloJenkinsController {
+	String message = "Its demo program has been deployed using Jenkins with Maven build on Tomcat7. Thanks!";
+ 
+	@RequestMapping("/hello")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+}
